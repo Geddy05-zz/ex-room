21658dbc6183b3f695af29c11244ede9ee1cc2ab
Geddy Schellevis <geddyschellevis@gmail.com>
2016-05-09 22:55:10 +0200

    first commit

---
+++ app/src/main/java/com/blend/mediamarkt/MainActivity.java
@@ -0,0 +1,112 @@
+package com.blend.mediamarkt;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
+
+import com.vuforia.DataSet;
+import com.vuforia.ObjectTracker;
+import com.vuforia.Tracker;
+import com.vuforia.TrackerManager;
+import com.vuforia.Vuforia;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOGTAG = "Media_Markt_Room";
+
+    private DataSet mCurrentDataset;
+    private Activity mActivity;
+    private Object mShutdownLock = new Object();
+    private InitVuforiaTask mInitVuforiaTask;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = this;
+        try
+        {
+            mInitVuforiaTask = new InitVuforiaTask();
+            mInitVuforiaTask.execute();
+        } catch (Exception e)
+        {
+            String logMessage = "Initializing Vuforia SDK failed";
+            Log.e(LOGTAG, logMessage);
+        }
+
+        int depthSize = 16;
+        int stencilSize = 0;
+        boolean translucent = Vuforia.requiresAlpha();
+
+//        mGlView = new SampleGLView(this);
+//        mGlView.init(translucent, depthSize, stencilSize);
+//
+//        mRenderer = new ImageTargetRenderer();
+//        mGlView.setRenderer(mRenderer);
+
+//        addContentView(mGlView, new LinearLayoutCompat.LayoutParams(LinearLayoutCompat.LayoutParams.MATCH_PARENT,
+//                LinearLayoutCompat.LayoutParams.MATCH_PARENT));
+    }
+
+    private void initTracker(){
+        TrackerManager tManager = TrackerManager.getInstance();
+        Tracker tracker = tManager.initTracker(ObjectTracker.getClassType());
+
+        ObjectTracker objectTracker = (ObjectTracker) tManager
+                .getTracker(ObjectTracker.getClassType());
+        mCurrentDataset = objectTracker.createDataSet();
+
+    }
+
+    private class InitVuforiaTask extends AsyncTask<Void, Integer, Boolean>
+    {
+        // Initialize with invalid value:
+        private int mProgressValue = -1;
+
+
+        protected Boolean doInBackground(Void... params)
+        {
+            // Prevent the onDestroy() method to overlap with initialization:
+            synchronized (mShutdownLock)
+            {
+                Vuforia.setInitParameters(mActivity, Vuforia.GL_20, "AerVtbn/////AAAAAZoEtTkaNUFVt33brNSVmuYLvikZecNvutuFTLvzPex9wdCATTeTtNn8XugB5UnFe/MHzBLjNMHZ4Bp2B6zI9AjTZkZYJtRUchvU25Xddk48nPvIm5Yk33wDdHl38IDGvh/J+SDy9GW64sWSbh8YYV3nCmC3KW0JHXRsBXy8OcSignR3Ede3xSQACCvLQynoY4NkHvm2VC0uDU83XBu3BFeyPk0Xp/c05CCkTgOtGycShbLdDavkCC3Vwmrm2u2NoaUkgfFCXDV2Dkbk5Uqr8dcJccGWJk3fZVs2orrZ5p4YenorSiDKXZMc4NFSxq4WmR/Vwx8XlqgCc3vzvLdvaZZ/+DgUhFNhQylAHuFyJtpO");
+
+                do
+                {
+                    mProgressValue = Vuforia.init();
+
+                    // Publish the progress value:
+                    publishProgress(mProgressValue);
+
+                } while (!isCancelled() && mProgressValue >= 0
+                        && mProgressValue < 100);
+
+                return (mProgressValue > 0);
+            }
+        }
+
+
+        protected void onProgressUpdate(Integer... values)
+        {
+            // Do something with the progress value "values[0]", e.g. update
+            // splash screen, progress bar, etc.
+        }
+
+
+        protected void onPostExecute(Boolean result)
+        {
+            if (result)
+            {
+
+            } else
+            {
+
+            }
+        }
+    }
+
+}
