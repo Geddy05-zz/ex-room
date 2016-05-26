b1b36972f04389437b70a2f5847b2e17726f6468
c4ndym4n <c4ndym4n2.0@gmail.com>
2016-05-26 09:58:15 +0200

    [ SETTINGS ] Global Settings

---
+++ app/src/main/java/com/blend/mediamarkt/utils/Settings.java
@@ -0,0 +1,7 @@
+package com.blend.mediamarkt.utils;
+
+/**
+ * Created by nimbus on 26/05/16.
+ */
+public class Settings {
+}
---
+++ usbcamera/src/main/java/com/nimbus/usbcamera/ActivityUsbCam.java
@@ -0,0 +1,13 @@
+package com.nimbus.usbcamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityUsbCam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usb_cam);
+    }
+}
