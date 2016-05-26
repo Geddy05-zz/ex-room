fa6e507e8dc3329c699cae701109246ed5e2dc00
Geddy Schellevis <geddyschellevis@gmail.com>
2016-05-26 09:21:01 +0200

    Add app settings

---
+++ app/src/main/java/com/blend/mediamarkt/AppSettings.java
@@ -0,0 +1,28 @@
+package com.blend.mediamarkt;
+
+/**
+ * Created by geddy on 26/05/16.
+ */
+
+public class AppSettings {
+
+    private boolean soundIsOn = true;
+    private boolean showCameraInBackground = false;
+
+    public void setSoundIsOn(){
+        soundIsOn = (soundIsOn) ? false : true;
+    }
+
+    public void setShowCameraInBackground(){
+        showCameraInBackground = (showCameraInBackground)? false : true;
+    }
+
+    public boolean isSoundIsOn(){
+        return soundIsOn;
+    }
+
+    public boolean isShowCameraInBackground (){
+        return showCameraInBackground;
+    }
+
+}
