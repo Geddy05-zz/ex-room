0fd6f3562e371ec989252047b0160787824c0cd6
Geddy Schellevis <geddyschellevis@gmail.com>
2016-05-30 21:37:38 +0200

    Wip: refactor

---
+++ app/src/main/java/com/blend/mediamarkt/VuforiaController.java
@@ -0,0 +1,197 @@
+package com.blend.mediamarkt;
+
+import android.graphics.Color;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.blend.mediamarkt.utils.exampleObject;
+import com.vuforia.CameraDevice;
+import com.vuforia.DataSet;
+import com.vuforia.ObjectTracker;
+import com.vuforia.STORAGE_TYPE;
+import com.vuforia.State;
+import com.vuforia.Trackable;
+import com.vuforia.Tracker;
+import com.vuforia.TrackerManager;
+
+import java.util.ArrayList;
+
+/**
+ * Created by geddy on 30-5-2016.
+ */
+public class VuforiaController implements ExRoomControl {
+
+    private static final String LOGTAG = "Media_Markt_Room";
+    private DataSet mCurrentDataset;
+    private int mCurrentDatasetSelectionIndex = 0;
+    private ArrayList<String> mDatasetStrings = new ArrayList<String>();
+    private boolean mExtendedTracking = false;
+    private exampleObject mRenderer;
+    private RelativeLayout mUILayout;
+
+    @Override
+    public boolean doInitTrackers() {
+        // Indicate if the trackers were initialized correctly
+        boolean result = true;
+
+        TrackerManager tManager = TrackerManager.getInstance();
+        Tracker tracker;
+
+        // Trying to initialize the image tracker
+        tracker = tManager.initTracker(ObjectTracker.getClassType());
+        if (tracker == null) {
+            Log.e(
+                    LOGTAG,
+                    "Tracker not initialized. Tracker already initialized or the camera is already started");
+            result = false;
+        } else {
+            Log.i(LOGTAG, "Tracker successfully initialized");
+        }
+        return result;
+    }
+
+    @Override
+    public boolean doLoadTrackersData() {
+        TrackerManager tManager = TrackerManager.getInstance();
+        ObjectTracker objectTracker = (ObjectTracker) tManager
+                .getTracker(ObjectTracker.getClassType());
+        if (objectTracker == null)
+            return false;
+
+        if (mCurrentDataset == null)
+            mCurrentDataset = objectTracker.createDataSet();
+
+        if (mCurrentDataset == null)
+            return false;
+
+        if (!mCurrentDataset.load(
+                mDatasetStrings.get(mCurrentDatasetSelectionIndex),
+                STORAGE_TYPE.STORAGE_APPRESOURCE))
+            return false;
+
+        if (!objectTracker.activateDataSet(mCurrentDataset))
+            return false;
+
+        int numTrackables = mCurrentDataset.getNumTrackables();
+        for (int count = 0; count < numTrackables; count++) {
+            Trackable trackable = mCurrentDataset.getTrackable(count);
+            if (isExtendedTrackingActive()) {
+                trackable.startExtendedTracking();
+            }
+
+            String name = "Current Dataset : " + trackable.getName();
+            trackable.setUserData(name);
+            Log.d(LOGTAG, "UserData:Set the following user data "
+                    + (String) trackable.getUserData());
+        }
+
+        return true;
+    }
+
+    boolean isExtendedTrackingActive() {
+        return mExtendedTracking;
+    }
+
+    @Override
+    public boolean doStartTrackers() {
+        // Indicate if the trackers were started correctly
+        boolean result = true;
+
+        Tracker objectTracker = TrackerManager.getInstance().getTracker(
+                ObjectTracker.getClassType());
+        if (objectTracker != null)
+            objectTracker.start();
+
+        return result;
+    }
+
+    @Override
+    public boolean doStopTrackers() {
+        // Indicate if the trackers were stopped correctly
+        boolean result = true;
+
+        Tracker objectTracker = TrackerManager.getInstance().getTracker(
+                ObjectTracker.getClassType());
+        if (objectTracker != null)
+            objectTracker.stop();
+
+        return result;
+    }
+
+    @Override
+    public boolean doUnloadTrackersData() {
+        // Indicate if the trackers were unloaded correctly
+        boolean result = true;
+
+        TrackerManager tManager = TrackerManager.getInstance();
+        ObjectTracker objectTracker = (ObjectTracker) tManager
+                .getTracker(ObjectTracker.getClassType());
+        if (objectTracker == null)
+            return false;
+
+        if (mCurrentDataset != null && mCurrentDataset.isActive()) {
+            if (objectTracker.getActiveDataSet().equals(mCurrentDataset)
+                    && !objectTracker.deactivateDataSet(mCurrentDataset)) {
+                result = false;
+            } else if (!objectTracker.destroyDataSet(mCurrentDataset)) {
+                result = false;
+            }
+
+            mCurrentDataset = null;
+        }
+
+        return result;
+    }
+
+    @Override
+    public boolean doDeinitTrackers() {
+        // Indicate if the trackers were deinitialized correctly
+        boolean result = true;
+
+        TrackerManager tManager = TrackerManager.getInstance();
+        tManager.deinitTracker(ObjectTracker.getClassType());
+
+        return result;
+    }
+
+    @Override
+    public void onInitARDone(ExRoomException exception) {
+        if (exception == null) {
+            initApplicationAR();
+
+            mRenderer.mIsActive = true;
+
+            // Now add the GL surface view. It is important
+            // that the OpenGL ES surface view gets added
+            // BEFORE the camera is started and video
+            // background is configured.
+            addContentView(mGlView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT));
+
+            // Sets the UILayout to be drawn in front of the camera
+            mUILayout.bringToFront();
+
+            // Sets the layout background to transparent
+            mUILayout.setBackgroundColor(Color.TRANSPARENT);
+
+
+            try {
+                vuforiaAppSession.startAR(CameraDevice.CAMERA_DIRECTION.CAMERA_DIRECTION_DEFAULT);
+            } catch (ExRoomException e) {
+                Log.e(LOGTAG, e.getString());
+            }
+
+            CameraDevice.getInstance().setFocusMode(
+                    CameraDevice.FOCUS_MODE.FOCUS_MODE_CONTINUOUSAUTO);
+
+        } else {
+            Log.e(LOGTAG, exception.getString());
+        }
+    }
+
+    @Override
+    public void onVuforiaUpdate(State state) {
+
+    }
+}
